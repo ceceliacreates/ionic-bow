a0047c6997388ae723661af897ac3db46ad4d8d7
ceceliacreates <ceceliacreates@gmail.com>
2020-10-24 08:19:33 -0400

    adds profile form with react hook form.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,86 @@
-import React from 'react'
-import { IonContent } from '@ionic/react'
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonInput,
+  IonItemDivider,
+  IonItem,
+  IonLabel,
+  IonButton,
+} from "@ionic/react";
+import { useForm, Controller } from "react-hook-form";
+
+type Inputs = {
+  username: string;
+  email: string;
+  location: string;
+};
 
 const ProfilePage: React.FC = () => {
-  return (
-    <IonContent class="ion-text-center">
-      <h1>Profile Page</h1>
-    </IonContent>
-  )
-}
+  const defaultProfile = {
+    username: "defaultUsername",
+    email: "defaultemail@email.com",
+    location: "Atlanta",
+  };
 
-export default ProfilePage
+  const { control, register, handleSubmit, watch, errors } = useForm<Inputs>();
+
+  const [profile, setProfile] = useState(defaultProfile);
+
+  const onSubmit = (updatedProfile: {
+    username: string;
+    email: string;
+    location: string;
+  }) => setProfile(updatedProfile);
+
+  return (
+    <IonContent>
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <IonItem>
+          <IonLabel position="fixed">Username</IonLabel>
+          <Controller
+            as={<IonInput></IonInput>}
+            control={control}
+            name="username"
+            defaultValue={profile.username}
+            rules={{ required: true }}
+          />
+        </IonItem>
+
+        {errors.username && <span>This field is required</span>}
+
+        <IonItem>
+          <IonLabel position="fixed">Email</IonLabel>
+          <Controller
+            as={<IonInput></IonInput>}
+            control={control}
+            name="email"
+            defaultValue={profile.email}
+            rules={{ required: true }}
+          />
+        </IonItem>
+
+        {errors.email && <span>This field is required</span>}
+
+        <IonItem>
+          <IonLabel position="fixed">Location</IonLabel>
+          <Controller
+            as={<IonInput></IonInput>}
+            control={control}
+            name="location"
+            defaultValue={profile.location}
+            rules={{ required: true }}
+          />
+        </IonItem>
+
+        {errors.location && <span>This field is required</span>}
+
+        <IonButton type="submit" expand="block">
+          Update Profile
+        </IonButton>
+      </form>
+    </IonContent>
+  );
+};
+
+export default ProfilePage;
